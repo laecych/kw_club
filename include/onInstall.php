96b1f7e0123d042ed3375166b6a0a74f02853e40
mambax7 <mambax7@gmail.com>
2019-04-15 09:54:36 -0400

    namespaces Utility

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,27 +1,16 @@
 <?php
 
+use XoopsModules\Tadtools;
+use XoopsModules\Kv_club\Utility;
+
 function xoops_module_install_kw_club(&$module)
 {
-    mk_group(_MI_KWCLUB_ADMIN_GROUP, _MI_KWCLUB_ADMIN_GROUP . _MI_KWCLUB_GROUP_NOTE);
-    mk_group(_MI_KWCLUB_TEACHER_GROUP, _MI_KWCLUB_TEACHER_GROUP . _MI_KWCLUB_GROUP_NOTE);
-    mk_dir(XOOPS_ROOT_PATH . '/uploads/kw_club');
-    mk_dir(XOOPS_ROOT_PATH . '/uploads/kw_club/class');
+    Utility::mk_group(_MI_KWCLUB_ADMIN_GROUP, _MI_KWCLUB_ADMIN_GROUP . _MI_KWCLUB_GROUP_NOTE);
+    Utility::mk_group(_MI_KWCLUB_TEACHER_GROUP, _MI_KWCLUB_TEACHER_GROUP . _MI_KWCLUB_GROUP_NOTE);
+    Tadtools\Utility::mk_dir(XOOPS_ROOT_PATH . '/uploads/kw_club');
+    Tadtools\Utility::mk_dir(XOOPS_ROOT_PATH . '/uploads/kw_club/class');
 
     return true;
 }
 
-function mk_group($name = '', $description = '')
-{
-    global $xoopsDB;
-    $sql = 'select groupid from ' . $xoopsDB->prefix('groups') . " where `name`='$name'";
-    $result = $xoopsDB->query($sql) or web_error($sql);
-    list($groupid) = $xoopsDB->fetchRow($result);
-    if (empty($groupid)) {
-        $sql = 'insert into ' . $xoopsDB->prefix('groups') . " (`name`, `description`) values('{$name}','{$description}')";
-        $xoopsDB->queryF($sql) or web_error($sql);
-        //取得最後新增資料的流水編號
-        $groupid = $xoopsDB->getInsertId();
-    }
 
-    return $groupid;
-}
--- include/onUninstall.php
+++ include/onUninstall.php
@@ -1,5 +1,8 @@
 <?php
 
+use XoopsModules\Tadtools;
+use XoopsModules\Kv_club\Utility;
+
 function xoops_module_uninstall_kw_club(&$module)
 {
     global $xoopsDB;
@@ -9,62 +12,5 @@ function xoops_module_uninstall_kw_club(&$module)
     return true;
 }
 
-//刪除目錄
-function delete_directory($dirname)
-{
-    if (is_dir($dirname)) {
-        $dir_handle = opendir($dirname);
-    }
 
-    if (!$dir_handle) {
-        return false;
-    }
 
-    while ($file = readdir($dir_handle)) {
-        if ('.' != $file && '..' != $file) {
-            if (!is_dir($dirname . '/' . $file)) {
-                unlink($dirname . '/' . $file);
-            } else {
-                delete_directory($dirname . '/' . $file);
-            }
-        }
-    }
-    closedir($dir_handle);
-    rmdir($dirname);
-
-    return true;
-}
-
-//拷貝目錄
-function full_copy($source = '', $target = '')
-{
-    if (is_dir($source)) {
-        if (!mkdir($target) && !is_dir($target)) {
-            throw new \RuntimeException(sprintf('Directory "%s" was not created', $target));
-        }
-        $d = dir($source);
-        while (false !== ($entry = $d->read())) {
-            if ('.' == $entry || '..' == $entry) {
-                continue;
-            }
-
-            $Entry = $source . '/' . $entry;
-            if (is_dir($Entry)) {
-                full_copy($Entry, $target . '/' . $entry);
-                continue;
-            }
-            copy($Entry, $target . '/' . $entry);
-        }
-        $d->close();
-    } else {
-        copy($source, $target);
-    }
-}
-
-function rm_group($name)
-{
-    $memberHandler = xoops_getHandler('member');
-    $group         = $memberHandler->createGroup();
-    $group->setVar('name', $name);
-    $memberHandler->insertGroup($group);
-}
