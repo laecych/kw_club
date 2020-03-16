c78c4f6d8e238bbbd381a4152730d1d246b89a88
Tad <tad0616@gmail.com>
2020-03-16 09:21:11 +0800

    針對無障礙 2.0 AA標準進行修正

---
+++ admin/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright    XOOPS Project (https://xoops.org)
+ * @license      http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package
+ * @since        2.5.0
+ * @author
+ * @version      $Id $
+ **/
+echo '<div align="center"><a href="https://xoops.org" target="_blank"><img src="../images/admin/xoopsmicrobutton.gif" alt="XOOPS" title="XOOPS"></a></div>';
+echo "<div class='center smallsmall italic pad5'><strong>" . $xoopsModule->name() . "</strong> is maintained by the <a class='tooltip' rel='external' href='https://xoops.org/' title='Visit XOOPS Community'>XOOPS Community</a></div>";
+
+xoops_cp_footer();
---
+++ admin/header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright    XOOPS Project (https://xoops.org)
+ * @license      http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package
+ * @since        2.5.0
+ * @author
+ * @version      $Id $
+ **/
+include dirname(__DIR__) . '/preloads/autoloader.php';
+require_once dirname(dirname(dirname(__DIR__))) . '/include/cp_header.php';
+
+xoops_loadLanguage('main', $xoopsModule->getVar('dirname'));
+
+if (!isset($xoopsTpl) || !is_object($xoopsTpl)) {
+    require_once XOOPS_ROOT_PATH . '/class/template.php';
+    $xoopsTpl = new \XoopsTpl();
+}
+
+xoops_cp_header();
+
+// Define Stylesheet and JScript
+$xoTheme->addStylesheet(XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/css/admin.css');
+//$xoTheme->addScript("browse.php?Frameworks/jquery/jquery.js");
+//$xoTheme->addScript("browse.php?modules/" . $xoopsModule->getVar("dirname") . "/js/admin.js");
