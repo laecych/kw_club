eca9c43af00bed15168b134eba684d6db0254b7e
laecych@gmail.com <laecych@gmail.com>
2020-05-12 11:33:49 +0800

    修改資料庫預設值

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -59,11 +59,7 @@ CREATE TABLE `kw_club_files_center` (
   `original_filename` varchar(255) NOT NULL DEFAULT '' COMMENT '檔案名稱',
   `hash_filename` varchar(255) NOT NULL DEFAULT '' COMMENT '加密檔案名稱',
   `sub_dir` varchar(255) NOT NULL DEFAULT '' COMMENT '檔案子路徑',
-<<<<<<< HEAD
   `upload_date` datetime NOT NULL COMMENT '上傳時間',
-=======
-  `upload_date` datetime NOT NULL COMMENT '上傳時間',
->>>>>>> bddd4aaa9a281fb2704e69acef0efbc413081143
   `uid` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '上傳者',
   `tag` varchar(255) NOT NULL DEFAULT '' COMMENT '註記',
   PRIMARY KEY (`files_sn`)
